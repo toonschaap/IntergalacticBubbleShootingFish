e78c078a9e63fa9d5d86912559b3e2b61bbda341
Toon <toonschaap3@gmail.com>
2019-09-17 13:34:28 +0200

    Game commit

---
+++ IntergalacticBubbleShootingFish/Assets/Script/player/PlayerMovement.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMovement : MonoBehaviour
+{
+	public float speed = 1.5f;
+    public Animator anim;
+//    public SpriteRenderer lol;
+
+//    private void Start()
+//    {
+//        anim = GetComponent<Animator>();
+//    }
+
+    private void Update ()
+	{
+		if (Input.GetKey(KeyCode.A))
+		{
+			transform.position += Vector3.left * speed * Time.deltaTime;
+        }
+		if (Input.GetKey(KeyCode.D))
+		{
+			transform.position += Vector3.right * speed * Time.deltaTime;
+		}
+	}
+}
---
+++ IntergalacticBubbleShootingFish/Assets/Script/sounds/shoot.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class shoot : MonoBehaviour {
+
+	public AudioSource someSound;
+
+
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKeyDown(KeyCode.F)){
+			someSound.Play();
+		}
+	}
+}
